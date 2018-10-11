6f0af091e3c455494572ba101907f3bfc46d1e8a
Nate Morris <nate.morris@marconet.com>
2018-10-11 09:04:07 -0500

    Add vscode

---
+++ third-party/vscode-extensions.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension donjayamanne.githistory
+code --install-extension eg2.vscode-npm-script
+code --install-extension esbenp.prettier-vscode
+code --install-extension felixfbecker.php-debug
+code --install-extension felixfbecker.php-intellisense
+code --install-extension felixfbecker.php-pack
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension HookyQR.beautify
+code --install-extension jspolancor.presentationmode
+code --install-extension mauve.terraform
+code --install-extension ms-python.python
+code --install-extension ms-vscode.csharp
+code --install-extension ms-vscode.Go
+code --install-extension ms-vscode.PowerShell
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension octref.vetur
+code --install-extension onecentlin.laravel-blade
+code --install-extension onecentlin.laravel-extension-pack
+code --install-extension onecentlin.laravel5-snippets
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension ryannaddy.laravel-artisan
+code --install-extension vuetifyjs.vuetify-vscode
+code --install-extension wwm.better-align
+
