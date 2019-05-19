ae9315cb68a10bf0cf98c4504dfbe9e8f215c630
Nate Morris <nate.morris@marconet.com>
2019-05-19 15:08:16 -0500

    Add ~/.tmux

--- third-party/mx-linux/tmux.sh
+++ third-party/mx-linux/tmux.sh
@@ -2,6 +2,8 @@
 
 sudo apt-get update && sudo apt-get install -y tmux
 
+mkdir -p $HOME/.tmux/plugins
+
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 
 $HOME/.tmux/plugins/tpm/bin/install_plugins
